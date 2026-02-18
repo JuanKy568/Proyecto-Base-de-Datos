051aec7e8057ef4cd823025a32d1b4bf0efda3e9
JuanKy568 <juandiegodiaz996@gmail.com>
2026-02-18 09:01:15 -0500

    Upload the two versions of the project

---
+++ MyFirstDBPry_JD/src/test/java/com/example/MyFirstDBPry_JD/MyFirstDbPryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstDBPry_JD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstDbPryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
